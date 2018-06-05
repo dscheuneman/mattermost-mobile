d56444fc63887a5b4665845584a28ff79c697db9
dscheuneman <35669036+dscheuneman@users.noreply.github.com>
2018-06-05 16:17:37 +0200

    Create zero.js

---
+++ app/components/zero/zero.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Zero extends PureComponent {
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text>Woot Component!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
